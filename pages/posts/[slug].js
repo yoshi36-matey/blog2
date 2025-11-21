5ac96275153db3a7a33ddc533e00609ecb16571c
Your Name <you@example.com>
2025-11-21 21:02:56 +0900

    initial commit

---
+++ pages/posts/[slug].js
@@ -0,0 +1,33 @@
+import { getPostData, getSortedPostsData } from '../../lib/posts';
+
+export async function getStaticPaths() {
+  const posts = getSortedPostsData();
+
+  const paths = posts.map((post) => ({
+    params: { slug: post.slug },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.slug);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <div>
+      <h1>{postData.title}</h1>
+      <p>{postData.date}</p>
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </div>
+  );
+}
---
+++ posts/hello.md
@@ -0,0 +1,9 @@
+yami
+
+---
+title: "Hello World"
+date: "2025-11-21"
+---
+#最初の投稿
+
+これは, Markdown で書いた記事です！
